b1835b01e43a1f7f8095ed26ba2975be3f9f90e0
Ashkaan Jaberi <ashkaan14@gmail.com>
2017-10-21 16:03:07 -0700

    done for saturday

--- index.js
+++ index.js
@@ -27,13 +27,30 @@ app.get('/api/notes', (req,res) => {
   let queryId = req.query.id ? {_id: mongodb.ObjectId(req.query.id)} : {};
   connection.then(db => {
     const col = promAll(db.collection('notes'));
-    col.findAsync(queryId).then(cur => {
+    col.findAsync(queryId).then(cur =
       promAll(cur).toArrayAsync()
         .then(res.send.bind(res))
         .catch(console.log)
         .catch(() => res.status(500).send('server error'));
+      return db;
     });
   });
+});
+
+app.delete('/api/notes', (req,res) => {
+  let queryId = req.query.id ? {_id: mongodb.ObjectId(req.query.id)} : false;
+  if(queryId){
+    connection.then(db => {
+      const col = promAll(db.collection('notes'));
+      col.removeAsync(queryId)
+        .then(() => res.status(204).send('note deleted!'))
+        .catch(console.log)
+        .catch(() => res.status(500).send('server error'));
+      return db;
+    });
+  } else {
+    res.status(400).send('no ID given');
+  }
 });
 
 app.listen(PORT, () => console.log(`server up on port: ${PORT}`));

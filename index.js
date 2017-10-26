5402a14886a82918b620cfbebbf27d74391b4541
Ashkaan Jaberi <ashkaan14@gmail.com>
2017-10-25 19:58:35 -0700

    halp

--- index.js
+++ index.js
@@ -1,61 +1,9 @@
 'use strict';
 
-const express = require('express');
-const jsonParser = require('body-parser').json();
-const promAll = require('bluebird').promisifyAll;
-const mongodb = require('mongodb');
-const MongoClient = promAll(mongodb.MongoClient);
-const connection = MongoClient.connectAsync('mongodb://localhost:27017/expressmongo');
+const app = require(__dirname + '/lib/server.js');
+
 require('dotenv').config();
 
-const app = express();
 const PORT = process.env.PORT || 3000;
- 
-app.post('/api/notes', jsonParser, (req,res) => {
-  connection.then(db => {
-    const col = promAll(db.collection('notes'));
-    col.insertAsync(req.body)
-      .then(mongoRes => mongoRes.ops[0])
-      .then(res.send.bind(res))
-      .catch(console.log)
-      .catch(() => res.status(500).send('server error'));
-    return db; 
-  });
-});
-
-app.get('/api/notes', (req,res) => {
-  let queryId = req.query.id ? {_id: mongodb.ObjectId(req.query.id)} : {};
-  connection.then(db => {
-    const col = promAll(db.collection('notes'));
-    col.findAsync(queryId).then(cur => {
-      promAll(cur).toArrayAsync()
-        .then(cur => {
-          if(cur.length===0) {
-            res.status(404).send('a note with that ID does not exist');
-          } 
-          res.status(200).send(cur);
-        })
-        .catch(console.log)
-        .catch(() => res.status(500).send('server error'));
-      return db;
-    });
-  });
-});
-
-app.delete('/api/notes', (req,res) => {
-  let queryId = req.query.id ? {_id: mongodb.ObjectId(req.query.id)} : false;
-  if(queryId){
-    connection.then(db => {
-      const col = promAll(db.collection('notes'));
-      col.removeAsync(queryId)
-        .then(() => res.status(204).send('note deleted!'))
-        .catch(console.log)
-        .catch(() => res.status(500).send('server error'));
-      return db;
-    });
-  } else {
-    res.status(400).send('no ID given');
-  }
-});
 
 app.listen(PORT, () => console.log(`server up on port: ${PORT}`));
--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,34 @@
-/* beforeAll, afterAll, expect */
+/* global beforeAll, afterAll, expect */
 
 'use strict';
 
+const superagent = require('superagent');
+const server = require(__dirname + '/../lib/server.js');
+
+describe('my express server', () => {
+  beforeAll( () => server.listen(3000));
+  afterAll( () => server.close());
+
+  describe('POST /api/notes', () => {
+    test('should respond with body content that was given', () => {
+      return superagent.post('http://localhost:3000/api/notes')
+        .send({'name':'gandalf','wizard':'heck ya'})
+        .then(res => {
+          expect(res.status).toEqual(200);
+          expect(res.body.name).toBe('gandalf');
+          expect(res.body.wizard).toBe('heck ya');
+        });
+    });
+    test('should respond with status 400 if improper JSON is receieved', () => {
+      return superagent.post('http://localhost:3000/api/notes')
+        .send({'derp':'halp'})
+        .then(res => {
+          expect(res.status).toEqual();
+        });
+    });
 
 
+  });
+
+
+});

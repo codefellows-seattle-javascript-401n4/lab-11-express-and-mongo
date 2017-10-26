ecbef1a4df4ccb1adc1d6a099dcc87037b0872cf
Ashkaan Jaberi <ashkaan14@gmail.com>
2017-10-26 15:43:37 -0700

    testing done

--- test/index.test.js
+++ test/index.test.js
@@ -5,6 +5,7 @@
 const superagent = require('superagent');
 const server = require(__dirname + '/../lib/server.js');
 
+
 describe('my express server', () => {
   beforeAll( () => server.listen(3000));
   afterAll( () => server.close());
@@ -23,12 +24,25 @@ describe('my express server', () => {
       return superagent.post('http://localhost:3000/api/notes')
         .send({'derp':'halp'})
         .then(res => {
-          expect(res.status).toEqual();
+          expect(res.status).toEqual(400);
         });
     });
-
-
   });
+
+  describe('GET /api/notes', () => {
+    test('should respond with 200 and the corresponding note', () => {
+      return superagent.get('http://localhost:3000/api/notes')
+        .then(res => {
+          expect(res.status).toEqual(200);
+        });
+    });
+    test('should respond with 404 if ID given does not exist', () => {
+      return superagent.get('http://localhost:3000/api/notes?id=59f260eb88057f1d73f1b143')
+        .then(res => {
+          expect(res.status).toEqual(404);
+        });
+    });
+  }); 
 
 
 });

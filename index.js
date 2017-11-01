3fd90b5751187acc20913b6df87265fc3a7c9ec5
dalhun namgung <bnamgung@gmail.com>
2017-11-01 13:10:50 -0700

    edit and organize

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 'use strict';
 
-require ('./lib/server.js').start();
-require ('').config();
+const PORT = process.env.PORT || 3000;
+const app = require ('./lib/routes.js');
+
+console.log (app);
+app.listen (PORT, () => {
+  console.log ('server on');
+});

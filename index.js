67e2ab6fc1765f28fff03661d103150ee1eba964
Darryl Davis <ddavis1306@gmail.com>
2017-11-01 14:59:59 -0700

    server is working get and post also work

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const app = require(__dirname + '/lib/server.js');
+
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log('server is connected on port: ' + PORT )); 

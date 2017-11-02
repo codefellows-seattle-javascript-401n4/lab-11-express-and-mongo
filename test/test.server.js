be63285175b468aec9310d9c6546bc4fcecf5b83
dalhun namgung <bnamgung@gmail.com>
2017-11-01 19:35:14 -0700

    almost

--- test/test.server.js
+++ test/test.server.js
@@ -7,7 +7,9 @@ describe ('api/notes', function () {
   beforeAll (server.start);
   afterAll (server.stop);
 
+
 describe ('POST'), function () {
+
   test ('main, name & 200 status', function () {
     return superagent.POST ('http://localhost:3000/api/notes')
     .set ('content')
@@ -21,6 +23,7 @@ describe ('POST'), function () {
       expect (res.main.content).toEqual ('content');
     });
 
+
     test ('error 400 no content', function () {
       return superagent.POST ('http://localhost:3000/api/notes')
       .set ('content', application/json)
@@ -31,8 +34,22 @@ describe ('POST'), function () {
       .catch (res => {
         expect (res.status).toEqual (400);
       });
-    }
-  })
 
-  }
-})
+
+  describe ('GET', function () {
+
+     test ('200 when request made', function () {
+       return superagent.get ('http://localhost:3000/api/notes')
+       .then (res => {
+         expect (res.status).toEqual (200);
+       })
+     })
+     test ('400 wrong id', function () {
+       return superagent.get ('http://localhost:3000/api/notes')
+       .then (Promise.reject)
+       .catch (res => {
+         expect (res.status).toEqual (400);
+       })
+     }}
+   });
+ }};

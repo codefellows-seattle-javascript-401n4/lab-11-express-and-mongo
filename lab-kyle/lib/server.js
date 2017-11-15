927a90ebf2dff214c770d5d06bfd66cd6c1e81f0
Kyle Richey <richey.kyle.i@gmail.com>
2017-11-15 09:26:09 -0800

    working on the last 2 routes

--- lab-kyle/lib/server.js
+++ lab-kyle/lib/server.js
@@ -1,48 +1,38 @@
 'use strict';
 
 const express = require('express');
+const Promise = require('bluebird');
+const jsonParser = require('body-parser').json();
+const prom = Promise.promisify;
+const promAll = Promise.promisifyAll;
+const MongoClient = promAll(require('mongodb').MongoClient);
+const connection = MongoClient.connectAsync('mongodb://localhost:27017/expressmongo');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
-const promAll = require('bluebird');
-const mongodb = require('mongodb');
-const MongoClient = promAll(mongodb.MongoClient);
-const connection = MongoClient.connectAsync('mongodb://localhost:27017/expressmongo');
-const jsonParser = require('body-parser').json();
-const Note = require('./note.js');
-
 
 app.post('/api/notes', jsonParser, (req, res) => {
-
-  if(!req.body.body){
-    return res.status(400).send('No Content');
-  }
-  if(!req.body.title){
-    return res.status(400).send('No Title');
-  }
-
-  let note = new Note(req.body);
-
   connection.then(db => {
-    const collection = promAll(db.collection('notes'));
-    collection.insertAsync(note)
-      .then(mongoRes => mongoRes.ops[0])
+    const col = promAll(db.collection('notes'));
+    col.insertAsync(req.body)
+      .then(mongoRes => mongoRes.op[0])
       .then(res.send.bind(res))
-      .catch(() => {
-        res.status(500).send('Error with the server');
-      });
+      .catch(console.log)
+      .catch(() => res.status(500)).send('server error');
     return db;
   });
 });
 
-app.get('/welcome', (req, res, next) => {
-  res.send('welcome page');
-  next();
-});
 
-app.use((req, res, next) => {
-  res.status(404).send('Path does not exist');
-  next();
-});
+// app.get('/api/notes', (req, res, next) => {
+//   res.send('welcome page');
+//   next();
+// });
+//
+// app.use((req, res, next) => {
+//   res.status(404).send('Path does not exist');
+//   next();
+// });
 
 app.listen(PORT, () => {
   console.log('Up and Running On PORT: ' + PORT);
